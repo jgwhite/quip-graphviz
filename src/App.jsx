2b88b394dfc9eb61924136bbab8b466e7b2535e6
Jamie White <jamie@jgwhite.co.uk>
2019-03-15 21:30:14 +0000

    Get some actual graphs rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,69 @@
-import Styles from "./App.less";
+import Viz from "viz.js";
+import { Module, render } from "viz.js/full.render.js";
 
 export default class App extends React.Component {
   render() {
+    let dot = this.props.rootRecord.get("dot");
+    let width = quip.apps.getContainerWidth();
+
     return (
-      <div>
+      <div
+        style={{
+          display: "flex",
+          width: `${width}px`
+        }}
+      >
+        <textarea
+          rows="20"
+          style={{
+            width: "50%",
+            fontFamily: "monospace",
+            border: "1px solid grey",
+            borderRadius: "0",
+            zIndex: 1
+          }}
+          onInput={e => this.updateDot(e.target.value)}
+        >
+          {dot}
+        </textarea>
+        <div
+          style={{
+            width: "50%",
+            border: "1px solid grey",
+            borderLeft: "0",
+            zIndex: 0
+          }}
+          ref={e => (this.graphContainer = e)}
+        />
       </div>
     );
+  }
+
+  updateDot(value) {
+    let { rootRecord } = this.props;
+    rootRecord.set("dot", value);
+    this.renderGraph();
+  }
+
+  componentDidMount() {
+    this.renderGraph();
+  }
+
+  componentDidUpdate() {
+    this.renderGraph();
+  }
+
+  async renderGraph() {
+    let viz = new Viz({ Module, render });
+    let dot = this.props.rootRecord.get("dot");
+    let svg = await viz.renderSVGElement(dot);
+    let div = this.graphContainer;
+    let old = div.firstChild;
+
+    if (old) {
+      div.replaceChild(svg, old);
+    } else {
+      div.appendChild(svg);
+    }
   }
 }
--- src/root.jsx
+++ src/root.jsx
@@ -1,13 +1,26 @@
 import quip from "quip";
 import App from "./App.jsx";
 
+export class RootRecord extends quip.apps.RootRecord {
+  static getProperties() {
+    return {
+      dot: "string"
+    };
+  }
+
+  static getDefaultProperties() {
+    return {
+      dot: "digraph {\n\n}"
+    };
+  }
+}
+
+quip.apps.registerClass(RootRecord, "root-record");
+
 quip.apps.initialize({
   initializationCallback(rootNode) {
     let rootRecord = quip.apps.getRootRecord();
 
-    ReactDOM.render(
-      <App rootRecord={rootRecord} />,
-      rootNode
-    );
+    ReactDOM.render(<App rootRecord={rootRecord} />, rootNode);
   }
 });
